3183dd76c35360ce47d55b82bdb88d7a70be0d47
NeDonil <svinoukhov03@gmail.com>
2023-11-09 16:28:31 +0300

    feat: add search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GroupsComponent } from './customer/groups/groups.component';
 import { CustomerComponent } from './customer/customer.component';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule} from '@angular/material/card';
+import { MatListModule} from '@angular/material/list';
+import { MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
 import { GroupItemComponent } from './customer/groups/group-item/group-item.component';
+import { MainComponent } from './customer/main/main.component';
+import { SearchComponent } from './customer/main/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GroupsComponent,
     CustomerComponent,
-    GroupItemComponent
+    GroupItemComponent,
+    MainComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatListModule,
-    MatIconModule
+    MatCardModule, MatListModule, MatIconModule,
+    FormsModule, MatFormFieldModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
