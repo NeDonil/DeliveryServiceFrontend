6b1665c3b9479bcdc27b7e1d4ad6b4b598b96447
NeDonil <svinoukhov03@gmail.com>
2023-11-18 19:03:49 +0300

    feat: fetch products from server

--- src/app/component/customer/main/main.component.ts
+++ src/app/component/customer/main/main.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/model/Product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html'
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy{
+    constructor(private productService: ProductService){}
 
+    products : Array<Product> | undefined;
+    productSubscription : Subscription | undefined;
+
+    ngOnInit() : void {
+        this.getProducts();
+    }
+
+    getProducts() :void{
+        this.productSubscription = this.productService
+            .getProductsInGroup(1)
+            .subscribe((_products) => {
+                this.products = _products;
+            })
+    }
+
+    ngOnDestroy(): void {
+        if(this.productSubscription){
+            this.productSubscription.unsubscribe();
+        }
+    }
 }
--- src/app/component/customer/main/product-cart/product-cart.component.ts
+++ src/app/component/customer/main/product-cart/product-cart.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/Product';
 
 @Component({
   selector: 'app-product-cart',
   templateUrl: './product-cart.component.html'
 })
-export class ProductCartComponent {
-
+export class ProductCartComponent{
+    @Input() product: Product | undefined;
 }
