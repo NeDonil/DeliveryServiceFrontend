e2310bb46cc150397251ca9204f847eadf558670
NeDonil <svinoukhov03@gmail.com>
2023-11-27 19:16:32 +0300

    fix: change Order to class instead of interface

--- src/app/model/Order.ts
+++ src/app/model/Order.ts
@@ -1,12 +1,12 @@
 import { Address } from "./Address";
 import { OrderItem } from "./OrderItem";
 
-export interface Order{
-    id : number ;
-    comment: string ;
-    address: Address ;
-    beginDate : Date;
-    endDate : Date;
-    status : string;
-    items: Array<OrderItem>;
+export class Order{
+    id : number | undefined ;
+    comment: string | undefined;
+    address: Address | undefined;
+    beginDate : Date | undefined;
+    endDate : Date | undefined;
+    status : string | undefined;
+    items: Array<OrderItem> | undefined;
 }
