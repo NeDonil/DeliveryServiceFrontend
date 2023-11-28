206f7f8e4337ca55def41d0f838274b209096de4
NeDonil <svinoukhov03@gmail.com>
2023-11-28 20:58:32 +0300

    feat: few improvements

--- src/app/model/OrderStatus.ts
+++ src/app/model/OrderStatus.ts
@@ -12,7 +12,7 @@ export const ORDER_STATUS_MAPPER : {[char: string]: string} = {
     [ORDER_STATUS.MAKING]: 'Делается',
     [ORDER_STATUS.PLACED]: 'Скоро начнем собирать!',
     [ORDER_STATUS.ASSEMBLING]: 'Собираем заказ!',
-    [ORDER_STATUS.ASSEMBLED]: 'Заказ собран. Ждем курьера!',
+    [ORDER_STATUS.ASSEMBLED]: 'Ждем курьера!',
     [ORDER_STATUS.DELIVERING]: 'Курьер уже в пути!',
     [ORDER_STATUS.DELIVERED]: `Доставлен`,
     [ORDER_STATUS.REJECTED]: 'Отменен',
