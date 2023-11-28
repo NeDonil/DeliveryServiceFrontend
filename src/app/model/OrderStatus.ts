3b1ecca6c5032101350714bd427fc135387c6a14
NeDonil <svinoukhov03@gmail.com>
2023-11-28 20:31:51 +0300

    feat: all order statuses

--- src/app/model/OrderStatus.ts
+++ src/app/model/OrderStatus.ts
@@ -1,13 +1,19 @@
 export enum ORDER_STATUS {
     MAKING = 'MAKING',
     PLACED = 'PLACED',
+    ASSEMBLING = 'ASSEMBLING',
+    ASSEMBLED = 'ASSEMBLED',
+    DELIVERING = 'DELIVERING',
     DELIVERED = 'DELIVERED',
     REJECTED = 'REJECTED'
 }
 
 export const ORDER_STATUS_MAPPER : {[char: string]: string} = {
     [ORDER_STATUS.MAKING]: 'Делается',
-    [ORDER_STATUS.PLACED]: 'Скоро начнем собирать',
+    [ORDER_STATUS.PLACED]: 'Скоро начнем собирать!',
+    [ORDER_STATUS.ASSEMBLING]: 'Собираем заказ!',
+    [ORDER_STATUS.ASSEMBLED]: 'Заказ собран. Ждем курьера!',
+    [ORDER_STATUS.DELIVERING]: 'Курьер уже в пути!',
     [ORDER_STATUS.DELIVERED]: `Доставлен`,
     [ORDER_STATUS.REJECTED]: 'Отменен',
 }
