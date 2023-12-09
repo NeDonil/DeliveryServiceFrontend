25acdc59d06dfed9b48390a88dd06ccf7ec84ac8
NeDonil <svinoukhov03@gmail.com>
2023-12-09 22:09:35 +0300

    fix(Dashboard): heavy db-requests drag n drop

--- src/app/component/dashboard/status-column/status-column.component.ts
+++ src/app/component/dashboard/status-column/status-column.component.ts
@@ -4,6 +4,7 @@ import {OrderWithEmployee} from "../../../model/OrderWithEmployee";
 import {Subscription} from "rxjs";
 import {AdminService} from "../../../service/admin.service";
 import {ORDER_ACTION_MAPPER} from "../../../model/OrderAction";
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-status-column',
@@ -39,6 +40,15 @@ export class StatusColumnComponent implements OnInit, OnDestroy{
             this.orders = this.orders?.filter(el => {
                 return el.order.id !== msg.order.id;
             });
+        }
+    }
+
+    drop(event: CdkDragDrop<OrderWithEmployee[]>) {
+        if (event.previousContainer !== event.container) {
+            const orderId = event.previousContainer.data[event.previousIndex].order.id;
+            if(orderId) {
+                this.adminService.updateOrderStatus(orderId, this.status);
+            }
         }
     }
 
